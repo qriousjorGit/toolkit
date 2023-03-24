23cd8bfad4702e68ced00ef5c0b03a0e152ec1fe
K <ielts.trainer.2010@gmail.com>
2023-03-24 15:44:54 -0400

    Big update added all CSS to static folder changed all .htmls to include CSS nopay.py - remove '?' character from search

--- nopaywall.py
+++ nopaywall.py
@@ -1,9 +1,12 @@
 import requests
 
 def get_free_link(target:str):
+    #remove ? from search strings
+    if "?" in target:
+        position = target.find("?")
+        target = target[:position]
+
     way_back_check = requests.get(f"http://archive.org/wayback/available?url={target}")
     clean_article = way_back_check.json()
     archive_link = (clean_article["archived_snapshots"]["closest"]["url"])
     return archive_link
-
-
